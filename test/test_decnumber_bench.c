2933cbb568a12b36dace2abc308a0358e79a398c
lifepillar <lifepillar@noemail.net>
2019-03-31 20:59:21 +0000

    Add an *incomplete* implementation based on decNumber. This encoded the decimals using the decPacked module. Compared to the implementation based on decQuad, this support a much wider range of decimals, but it's also slower. The used encoding is not lexicographically sorted. I do not plan to follow this development line any further.
    FossilOrigin-Name: ad39931d07164703eaaf0e569e22d0afac80681acfbd5fcd2acc2cd8ca0bd661

--- test/test_decnumber_bench.c
+++ test/test_decnumber_bench.c
@@ -1,10 +1,9 @@
 static sqlite3* db;
 
-static int
-sqlite3_decquad_test_decsum_benchmark(void) {
+static int sqlite3_decnumber_test_numsum_benchmark(void) {
   mu_db_execute(db, "drop table if exists T;");
   mu_db_execute(db, "create table T(n int, d blob);");
-  char cmd[] = "insert into T(n,d) values (?1,dec(?2))";
+  char cmd[] = "insert into T(n,d) values (?1,num(?2))";
   sqlite3_stmt* stmt;
   sqlite3_prepare_v2(db, cmd, (int)strlen(cmd), &stmt, 0);
   sqlite3_bind_text(stmt, 2, "0.000001", -1, SQLITE_STATIC);
@@ -16,19 +15,20 @@ sqlite3_decquad_test_decsum_benchmark(void) {
     sqlite3_reset(stmt);
   }
   sqlite3_finalize(stmt);
-  mu_benchmark_query(db, "select decStr(decSum(d)) from T", 10);
+  mu_benchmark_query(db, "select numStr(numSum(d)) from T", 10);
   mu_assert_query(db, "select max(n) from T", "999999");
-  mu_assert_query(db, "select decStr(decSum(d)) from T", "1.000000");
-  mu_assert_query(db, "select decStr(decMax(d)) from T", "0.000001");
-  mu_assert_query(db, "select decStr(decMin(d)) from T", "0.000001");
-  mu_assert_query(db, "select decStr(decAvg(d)) from T", "0.000001");
+  mu_assert_query(db, "select numStr(numSum(d)) from T", "1.000000");
+  mu_assert_query(db, "select numStr(numMax(d)) from T", "0.000001");
+  mu_assert_query(db, "select numStr(numMin(d)) from T", "0.000001");
+  mu_assert_query(db, "select numStr(numAvg(d)) from T", "0.000001");
   return 0;
 }
 
 static void
-sqlite3_decquad_bench_tests() {
+sqlite_decnumber_bench_tests() {
   mu_setup = mu_noop;
   mu_teardown = mu_noop;
 
-  mu_test(sqlite3_decquad_test_decsum_benchmark);
+  mu_test(sqlite3_decnumber_test_numsum_benchmark);
 }
+
