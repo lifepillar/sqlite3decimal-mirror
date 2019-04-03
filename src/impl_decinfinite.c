bcefba1e55bada9c117df33b2a85e76e9b594ec5
lifepillar <>
2019-04-03 12:55:41 +0000

    Add license information to all files.
    FossilOrigin-Name: 50ebff18a524525da2ef6d1cc47c898383a5cabd92961af1d329d9032378fda0

--- src/impl_decinfinite.c
+++ src/impl_decinfinite.c
@@ -1,5 +1,13 @@
 /**
  * \file impl_decinfinite.c
+ * \author    Lifepillar
+ * \copyright Copyright (c) 2019 Lifepillar.
+ *            This program is free software; you can redistribute it and/or
+ *            modify it under the terms of the Simplified BSD License (also
+ *            known as the "2-Clause License" or "FreeBSD License".)
+ * \copyright This program is distributed in the hope that it will be useful,
+ *            but without any warranty; without even the implied warranty of
+ *            merchantability or fitness for a particular purpose.
  *
  * \brief Implementation of decimal arithmetic using decNumber and the
  *        decimalInfinite encoding.
